9eddf4a2e0150747521766aa283d9a9c8a795770
1mru <souichi.ueda@gmail.com>
2026-09-19 14:46:16 +0100

    メニューバーを追加

--- main.cpp
+++ main.cpp
@@ -2,6 +2,27 @@
 #include <richedit.h>
 #include <shellscalingapi.h>
 
+// ファイル
+#define IDM_NEW         101
+#define IDM_OPEN        102
+#define IDM_SAVE        103
+#define IDM_SAVEAS      104
+#define IDM_CLOSE       105
+#define IDM_CLOSEALL    106 
+#define IDM_QUIT        107
+
+// 編集
+#define IDM_UNDO        201
+#define IDM_REDO        202
+#define IDM_CUT         203
+#define IDM_COPY        204
+#define IDM_PASTE       205
+#define IDM_DELETE      206
+#define IDM_SELECTALL   207
+
+// ヘルプ
+#define IDM_ABOUT       301
+
 HWND hwndEdit = NULL;
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -33,6 +54,35 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
 
   RegisterClass(&wc);
 
+
+  HMENU hFileMenu = CreateMenu();
+  AppendMenuW(hFileMenu, MF_STRING, IDM_NEW, L"新規作成(&N)\tCtrl+N");
+  AppendMenuW(hFileMenu, MF_STRING, IDM_OPEN, L"開く(&O)...\tCtrl+O");
+  AppendMenuW(hFileMenu, MF_STRING, IDM_SAVE, L"保存(&S)\tCtrl+S");
+  AppendMenuW(hFileMenu, MF_STRING, IDM_SAVEAS, L"名前を付けて保存(&A)\tCtrl+Shift+S");
+  AppendMenuW(hFileMenu, MF_STRING, IDM_CLOSE, L"閉じる(&C)\tCtrl+W");
+  AppendMenuW(hFileMenu, MF_STRING, IDM_CLOSEALL, L"すべて閉じる(&E)\tCtrl+Shift+W");
+  AppendMenuW(hFileMenu, MF_SEPARATOR, 0, 0);
+  AppendMenuW(hFileMenu, MF_STRING, IDM_QUIT, L"終了(&X)\tCtrl+Q");
+
+  HMENU hEditMenu = CreateMenu();
+  AppendMenuW(hEditMenu, MF_STRING, IDM_UNDO, L"元に戻す(&U)\tCtrl+Z");
+  AppendMenuW(hEditMenu, MF_STRING, IDM_REDO, L"やり直し(&R)\tCtrl+R");
+  AppendMenuW(hEditMenu, MF_SEPARATOR, 0, 0);
+  AppendMenuW(hEditMenu, MF_STRING, IDM_CUT, L"切り取り(&T)\tCtrl+X");
+  AppendMenuW(hEditMenu, MF_STRING, IDM_COPY, L"コピー(&C)\tCtrl+C");
+  AppendMenuW(hEditMenu, MF_STRING, IDM_PASTE, L"貼り付け(&P)\tCtrl+V");
+  AppendMenuW(hEditMenu, MF_STRING, IDM_DELETE, L"削除(&D)\tDel");
+  AppendMenuW(hEditMenu, MF_STRING, IDM_SELECTALL, L"すべて選択(&A)\tCtrl+A");
+
+  HMENU hHelpMenu = CreateMenu();
+  AppendMenuW(hHelpMenu, MF_STRING, IDM_ABOUT, L"バージョン情報(&A)...");
+
+  HMENU hMainMenu = CreateMenu();
+  AppendMenuW(hMainMenu, MF_POPUP, reinterpret_cast<UINT_PTR>(hFileMenu), L"ファイル(&F)");
+  AppendMenuW(hMainMenu, MF_POPUP, reinterpret_cast<UINT_PTR>(hEditMenu), L"編集(&E)");
+  AppendMenuW(hMainMenu, MF_POPUP, reinterpret_cast<UINT_PTR>(hHelpMenu), L"ヘルプ(&H)");
+
   HWND hwnd = CreateWindowExW(
     0,                            // 追加スタイル
     CLASS_NAME,                   // ウィンドウクラス
@@ -41,7 +91,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
     CW_USEDEFAULT, CW_USEDEFAULT, // 大きさ
     CW_USEDEFAULT, CW_USEDEFAULT, // 位置
     NULL,                         // 親
-    NULL,                         // メニュー
+    hMainMenu,                    // メニュー
     hInstance,                    // インスタンス
     NULL                          // 追加データ
   );
